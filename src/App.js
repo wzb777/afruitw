fa8b4b91312b02574d2ba898e63df869391b67e7
wzb777 <18833219092@163.com>
2018-12-15 20:49:12 +0800

    上传项目框架

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./common/css/style.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         	<i>menu</i>
+         	<h1>LOGO</h1>
+         	<p>搜索</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "./common/css/reset.css";
+import "./common/js/flexble"
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
