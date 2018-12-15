8a26ccc0d4e74d5a95f455caca44e99d1a5d9895
wzb777 <18833219092@163.com>
2018-12-15 21:24:06 +0800

    添加了底部

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./common/css/style.css"
-
+import "./common/css/reset.css"
 class App extends Component {
   render() {
     return (
@@ -10,6 +10,14 @@ class App extends Component {
          	<h1>LOGO</h1>
          	<p>搜索</p>
         </header>
+        <footer>
+        		<ul>
+        				<li>首页</li>
+        				<li>列表</li>
+        				<li>我的</li>
+        				<li>登陆</li>
+        		</ul>
+        </footer>
       </div>
     );
   }
